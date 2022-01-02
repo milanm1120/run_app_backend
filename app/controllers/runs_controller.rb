cbc082ccfba841ff6126c2622a9a5b97e8054ba5
Milan <milanm1120@gmail.com>
2022-01-02 13:59:32 -0500

    Rails API refactoring, working

--- app/controllers/runs_controller.rb
+++ app/controllers/runs_controller.rb
@@ -1,32 +1,23 @@
 class RunsController < ApplicationController
     def index
         runs = Run.all
-        render json: runs
+        render({json: runs, except: [:created_at, :updated_at]})
     end
 
     def create
         run = Run.new(run_params)
         # byebug
         if run.save
-            render json: run
+            render ({json: run})
         else
             render json: {error: "Run Not Saved"}
-        end
-    end
-
-    def update
-        run = Run.find_by_id(params[:id])
-        if run.update(run_params)
-            render json: run
-        else
-            render json: {error: "Unable to Update"}
         end
     end
 
     def destroy
         run = Run.find_by_id(params[:id])
         run.destroy
-        render json: {messsage: "Successfully Deleted #{run.run_type}"}
+        render json: {messsage: "Successfully Deleted"}
     end
     private
 
